e30abe89e2d00048b2763cef1fb06aa81736b82f
doncorley <don@tourgeek.com>
2011-03-02 08:09:51 -0800

    Add standalone backup program

---
+++ org.jbundle.util.backup/src/main/java/org/jbundle/util/backup/Backup.java
@@ -0,0 +1,15 @@
+package org.jbundle.util.backup;
+
+import org.jbundle.jbackup.JBackup;
+
+/**
+ * Hello world!
+ *
+ */
+public class Backup extends JBackup
+{
+    public static void main( String[] args )
+    {
+    	JBackup.main(args);
+    }
+}
